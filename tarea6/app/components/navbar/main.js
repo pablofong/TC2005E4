b2d54c08c8e1895db19370d98f2718942a83ca60
pablofong <pablo_fong@hotmail.com>
2023-03-09 01:50:14 -0700

    #t6-4-routing-and-navbar-created

---
+++ tarea6/app/components/navbar/main.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class NavbarComponent extends Component {
+  // component code goes here
+}
--- tarea6/ember-cli-build.js
+++ tarea6/ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    minifyCSS: {
+      options: { processImport: true },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +22,11 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  // Import Bootstrap CSS
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
+
+  // Import Bootstrap JavaScript
+  app.import('node_modules/bootstrap/dist/js/bootstrap.js');
 
   return app.toTree();
 };
