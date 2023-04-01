81dbef8f25814b1b8cbf4baf9ce4a8b45abfcaa8
pabloamtzs <pabloamtz00@hotmail.com>
2023-03-31 18:31:56 -0700

    update-products-page-add-json-and-js-to-products-

--- tarea6/app/routes/products.js
+++ tarea6/app/routes/products.js
@@ -1,2 +1,10 @@
 import Route from '@ember/routing/route';
-export default class ProductsRoute extends Route {}
+
+export default class ProductsRoute extends Route {
+    async model() {
+        const response = await fetch('/data.json');
+        const data = await response.json();
+        return data;
+      }
+}
+
